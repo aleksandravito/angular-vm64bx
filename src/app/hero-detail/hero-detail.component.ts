e65bbe64189ac230550da636021eb663e591b9f3
aleksandravito <aleksandra.vito@keminet.net>
2021-06-24 16:14:14 +0200

    2 files modified

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -14,24 +14,10 @@ export class HeroDetailComponent implements OnInit {
   ngOnInit() {}
 
   save(hero: Hero) {
-    // localStorage.setItem('heroes_list', JSON.stringify(hero));
-    // this.storage.put(this.storage.heroList, hero);
-    //Find index of specific object using findIndex method.
-    let objIndex = this.storage.heroList.findIndex(obj => obj.id == hero.id);
-
-    //Log object to Console.
-    console.log('Before update: ', this.storage.heroList[objIndex]);
-
-    //Update object's name property.
-    this.storage.heroList[objIndex].name = hero.name;
-
-    //Log object to console again.
-    console.log('After update: ', this.storage.heroList[objIndex]);
-
-    this.storage.put(hero, hero);
-    console.log(this.storage.heroList.indexOf(hero));
-    console.log(this.storage.heroList);
-    // return this.storage.put(this.storage, hero);
+    return this.storage.put(hero, hero);
+    // console.log(this.storage.heroList.indexOf(hero));
+    // console.log(this.storage.heroList);
+    // console.log(typeof localStorage.getItem('heroes_list'));
   }
 }
 
--- src/app/heroes-storage.service.ts
+++ src/app/heroes-storage.service.ts
@@ -1,27 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 const storageName = 'heroes_list';
 
-const HEROES = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Torn' }
-];
-
 @Injectable()
 export class HeroesStorageService {
-  private heroList;
+  heroList;
 
   constructor() {
-    this.heroList = HEROES;
+    this.heroList = JSON.parse(localStorage.getItem(storageName)) || HEROES;
   }
 
   // get items
